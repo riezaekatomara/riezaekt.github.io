d668f1562a78579f9775a0c353b6395c2c23d97f
riezaekatomara <130181217+riezaekatomara@users.noreply.github.com>
2024-12-22 20:09:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function pemain() {
+  window.open("http://127.0.0.1:5500/pemain.htm");
+}
+function klub() {
+  window.open("http://127.0.0.1:5500/klub.htm");
+}
+function negara() {
+  window.open("http://127.0.0.1:5500/negara.htm");
+}
+function ml() {
+  window.open("http://127.0.0.1:5500/ml.htm");
+}
+function naruto() {
+  window.open("http://127.0.0.1:5500/naruto.htm");
+}
